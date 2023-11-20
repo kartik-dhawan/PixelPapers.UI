aeca56c6d1e264bfac0c285cc283e5563f0d8bde
kartik-dhawan <kartikkdhawan@gmail.com>
2023-11-20 20:28:11 +0530

    PP-10: Deleted unwanted files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Dummy from "@/components/Dummy"
 import ReduxProvider from "@/redux/ReduxProvider"
 
 export default function Home() {
   return (
     <main>
       <ReduxProvider>
-        <Dummy />
+        <div>Hello!!!</div>
       </ReduxProvider>
     </main>
   )
