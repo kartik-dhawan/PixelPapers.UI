284cff1fb5763cc7fa709c9ca10b5e4c19917a39
kartik-dhawan <kartikkdhawan@gmail.com>
2023-11-23 00:27:03 +0530

    DEV: Added dynamic metadata for /travel/[slug] pages

--- src/app/(blog)/travel/[slug]/page.tsx
+++ src/app/(blog)/travel/[slug]/page.tsx
@@ -1,8 +1,32 @@
 import TravelBlogPosts from "@/components/TravelBlogPosts"
 import { getTravelBlogBySlug } from "@/lib/methods"
 import ReduxProvider from "@/redux/ReduxProvider"
+import { Metadata, ResolvingMetadata } from "next"
 
-const TravelBlogPage = async ({ params }: { params: { slug: string } }) => {
+interface TravelSlugParams {
+  params: { slug: string }
+  parent?: ResolvingMetadata
+}
+
+export async function generateMetadata({
+  params,
+  parent,
+}: TravelSlugParams): Promise<Metadata> {
+  const currentBlog = await getTravelBlogBySlug(params?.slug ?? "")
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent)?.openGraph?.images || []
+
+  return {
+    title: currentBlog?.blogTitle ?? "Travel Blog | Pixel Papers",
+    description: "",
+    openGraph: {
+      images: [currentBlog?.blogMetaImage.fields.file.url, ...previousImages],
+    },
+  }
+}
+
+const TravelBlogPage = async ({ params }: TravelSlugParams) => {
   // gets current blog directly from contentful
   const currentBlog = await getTravelBlogBySlug(params?.slug ?? "")
 
