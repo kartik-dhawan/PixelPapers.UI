5ff4a60244853ccffe733984d322543790912621
kartik-dhawan <kartikkdhawan@gmail.com>
2023-11-24 21:48:11 +0530

    PP-19: Added show/hide button for blog-nav

--- src/components/blogposts/BlogSideNavigation/index.tsx
+++ src/components/blogposts/BlogSideNavigation/index.tsx
@@ -1,9 +1,20 @@
+"use client"
+
 import { dmSans } from "@/utils/fonts"
 import { TravelBlogContentfulObject } from "@/utils/interfaces"
-import { Box, Divider, List, ListItem, Stack } from "@mui/material"
+import {
+  Box,
+  Button,
+  Collapse,
+  Divider,
+  List,
+  ListItem,
+  Stack,
+} from "@mui/material"
 import { styles } from "./styles"
 import Link from "next/link"
 import { lightenHexColor } from "@/utils/methods"
+import { useCallback, useState } from "react"
 
 interface BlogSideNavigationProps {
   blogType: string
@@ -16,33 +27,59 @@ const BlogSideNavigation = ({
   themeColor,
   blogs,
 }: BlogSideNavigationProps) => {
+  const [showBlogLinks, setShowBlogLinks] = useState(true)
+
+  const showHideToggleHandler = useCallback(() => {
+    setShowBlogLinks((prev) => !prev)
+  }, [])
+
   return (
     <Stack
       divider={<Divider sx={styles.blogsNavDivider} />}
       sx={styles.blogsSideNavWrapper}
       className={dmSans.className}
     >
-      <Box sx={styles.blogsSideNavTitle}>More on {blogType}</Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Box sx={styles.blogsSideNavTitle}>More on {blogType}</Box>
+        <Button
+          disableRipple
+          sx={{
+            padding: 0,
+            textTransform: "capitalize",
+            transition: "200ms all ease",
+            "&:hover": {
+              backgroundColor: "transparent",
+              textDecoration: "underline",
+              opacity: 0.7,
+            },
+            color: themeColor,
+          }}
+          onClick={showHideToggleHandler}
+        >
+          {showBlogLinks ? "Hide" : "Show"}
+        </Button>
+      </Stack>
       <List sx={{ display: "flex", flexDirection: "column", gap: "6px" }}>
         {blogs.map((item) => {
           return (
-            <ListItem
-              key={item.sys.id}
-              sx={{
-                ...styles.blogNavListItem,
-                backgroundColor: lightenHexColor(themeColor ?? "#222", 0.9),
-                "&:hover": {
-                  backgroundColor: lightenHexColor(themeColor ?? "#222", 0.8),
-                },
-              }}
-            >
-              <Link
-                href={`/travel/${item.fields.blogUrlSlug}`}
-                aria-label={`Click here to go to another article named: ${item.fields.blogTitle}`}
+            <Collapse in={showBlogLinks} key={item.sys.id}>
+              <ListItem
+                sx={{
+                  ...styles.blogNavListItem,
+                  backgroundColor: lightenHexColor(themeColor ?? "#222", 0.9),
+                  "&:hover": {
+                    backgroundColor: lightenHexColor(themeColor ?? "#222", 0.8),
+                  },
+                }}
               >
-                {item.fields.blogTitle}
-              </Link>
-            </ListItem>
+                <Link
+                  href={`/travel/${item.fields.blogUrlSlug}`}
+                  aria-label={`Click here to go to another article named: ${item.fields.blogTitle}`}
+                >
+                  {item.fields.blogTitle}
+                </Link>
+              </ListItem>
+            </Collapse>
           )
         })}
       </List>
