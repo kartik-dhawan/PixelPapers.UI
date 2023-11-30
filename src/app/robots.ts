bf22116a7d5e6bad4d7e7ed75d16d092a60c0dd6
kartik-dhawan <kartikkdhawan@gmail.com>
2023-11-30 17:57:02 +0530

    PP-30: Added sitemaps & robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,15 @@
+import { MetadataRoute } from "next"
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow:
+        process.env.NEXT_PUBLIC_ENVIRONMENT === "dev"
+          ? ["/api/", "/not-found.tsx"]
+          : "/",
+    },
+    sitemap: "https://kartikdhawan.in/sitemap.xml",
+  }
+}
