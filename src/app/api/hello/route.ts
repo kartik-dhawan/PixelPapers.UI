3001c4f1e933cf248fec0adbf94ed39ec14f7f62
kartik-dhawan <kartikkdhawan@gmail.com>
2024-02-23 18:12:44 +0530

    graphql-demo: added headers for CORS

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,16 @@
 import { NextResponse } from "next/server"
 
 export async function GET() {
-  return NextResponse.json({ name: "hello" })
+  return NextResponse.json(
+    { name: "hello" },
+    // {
+    //   headers: {
+    //     "Access-Control-Allow-Origin": "*",
+    //     "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT",
+    //     "Access-Control-Allow-Headers":
+    //       "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    //     "Access-Control-Allow-Credentials": "true",
+    //   },
+    // },
+  )
 }
